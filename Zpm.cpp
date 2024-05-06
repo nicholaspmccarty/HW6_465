9b15b23faf82bc735fbd51cceff84addef07107e
mccartnp <nicholasmccarty252@gmail.com>
2024-05-06 11:50:44 -0400

    Added helper function

---
+++ Zpm.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <filename>.zpm" << std::endl;
+        return 1;
+    }
+}
+
+   
+
+
+
+void parseAndExecute(const std::string& filename) {
+}
